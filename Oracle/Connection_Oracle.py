8fefbcebd602b1ff31cfb9732c40882d1b109e94
straybe <enrique66672@gmail.com>
2023-05-23 16:33:24 -0600

    Agregado de logo y clase conexion a la base de datos

--- Oracle/Connection_Oracle.py
+++ Oracle/Connection_Oracle.py
@@ -1,15 +1,42 @@
 import cx_Oracle
-class User:
 
-    def Connection(usuario, contraseña):
+class Connection_Oracle:
+
+    def __init__(self):
+        self.user = None
+        self.password = None
+        self.dsn = 'localhost:1521/XEPDB1'
+        self.encoding = 'UTF-8'
+        self.connection = None
+
+    def open(self, user, password):
+        self.user = user
+        self.password = password
+
         try:
-            connection = cx_Oracle.connect(
-                user = usuario,
-                password = contraseña,
-                dsn = 'localhost:1521/XEPDB1',
-                encoding = 'UTF-8'
-            )
-            print("CONECTADO!")
+            self.connection = cx_Oracle.connect(self.user, self.password, self.dsn, self.encoding)
+            print('Successful Connection')
+        except cx_Oracle.DatabaseError as e:
+            error = e.args
+            print('Database connection error', error.message)
+    
+    def close (self):
+        if (self.connection): # Si hay conexion, desconectar y volver a colocar None
+            self.connection.close()
+            print('Connection Finished')
+            self.connection = None
+        else:
+            print('There is not connection')
 
-        except Exception as ex:
-            print(ex)
+    def query_execution (self, consulta):
+        if (self.connection):
+            cursor = self.connection.cursor()
+            cursor.execute(consulta)
+            # Obtiene todos los registros resultantes
+            result = cursor.fechall()
+            self.connection.commit()
+            cursor.close()
+
+            return result
+        else:
+            print('There is not connection')
