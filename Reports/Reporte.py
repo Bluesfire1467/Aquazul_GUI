aba6d00ab81d3e3e5b7d700cc0595c045c8586aa
straybe <enrique66672@gmail.com>
2023-06-16 06:46:05 -0600

    Se agregaron reportes

--- Reports/Reporte.py
+++ Reports/Reporte.py
@@ -1,98 +1,130 @@
-import os.path
-import subprocess
+import csv
+import os
 import cx_Oracle
-from Oracle.Connection_Oracle import Connection_Oracle
+from agatereports.basic_report import BasicReport
+import webbrowser
+import logging
 
-def generate_report(name_file, conn):
-    jasper_starter_path = "./Program Files (x86)/JasperStarter"
-    jrxml_file = open(f'./{name_file}.jrxml', 'w')
-    jrxml_file.close()
-    jasper_file = open(f'./{name_file}.jasper', 'w')
-    jasper_file.close()
-    output_file = open(f'./{name_file}.pdf', 'w')
-    output_file.close()
+logger = logging.getLogger(__name__)
 
 
-
-    db_params = {
-        "host": "localhost",
-        "port": "1521",
-        "database": "xepdb1",
-        "user": conn.user,
-        "password": conn.password
-    }
-
-    compile_comand = [
-        jasper_starter_path,
-        "compile",
-        jrxml_file,
-        "-o",
-        jasper_file
-    ]
-
-    generate_command = [
-        jasper_starter_path,
-        "printraw",
-        jasper_file,
-        "-f",
-        "PDF",
-        "-o",
-        output_file,
-        "-t",
-        "oracle",
-        "-u",
-        db_params["user"],
-        "-p",
-        db_params["password"],
-        "-H",
-        db_params["host"],
-        "-n",
-        db_params["port"],
-        "-n",
-        db_params["database"]
-    ]
-
-    subprocess.run(compile_comand)
-
-    subprocess.run(generate_command)
-
-#conn = Connection_Oracle()
-#conn.user = "Developer"
-#conn.password = "PassDev"
-#name_file = "reporte"
-#generate_report(name_file, conn)
-
-def generar_reporte(ruta_jrxml):
-    cx_Oracle.init_oracle_client(lib_dir = 'ojdbc8.jar')
+def create_connection():
     connection = cx_Oracle.connect(
-                user='developer',
-                password='PassDev',
-                dsn='localhost:1521/XEPDB1',
-                encoding='UTF-8')
+        user='developer',
+        password='PassDev',
+        dsn='localhost:1521/XEPDB1',
+        encoding='UTF-8'
+    )
+    return connection
 
+
+def reporte_habitad(habitad):
+    connection = create_connection()
     cursor = connection.cursor()
-    query = '''SELECT NOMBRE_HABITAT, COUNT(NOMBRE_ANIMAL) AS ANIMALES
-               FROM ANIMAL
-               JOIN HABITAT USING(ID_HABITAT)
-               GROUP BY NOMBRE_HABITAT
-               ORDER BY ANIMALES'''
 
-    query = '''SELECT * FROM EMPLEADO'''
-    cursor.execute(str(query))
+    query = str(f'''SELECT NOMBRE_ANIMAL, 
+                    NOMBRE_COMUN, 
+                    NOMBRE_CIENTIFICO 
+                    FROM DEVELOPER.ANIMAL
+                    JOIN DEVELOPER.ESPECIE USING(ID_ESPECIE)
+                    JOIN DEVELOPER.HABITAT USING(ID_HABITAT)
+                    WHERE NOMBRE_HABITAT LIKE \'{habitad}\'''')
 
-    with open("datos.csv", "w") as archivo_csv:
-        for row in cursor:
-            archivo_csv.write(','.join([str(x) for x in row]) + '\n')
+    cursor.execute(query)
+    resultados = cursor.fetchall()
+    print(resultados)
+    archivo_csv = 'temp.csv'
+    with open(archivo_csv, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['NOMBRE_ANIMAL', 'NOMBRE_COMUN', 'NOMBRE_CIENTIFICO'])
+        for row in resultados:
+            writer.writerow(row)
 
-    comando = f"jasperstarter pr {ruta_jrxml} -f csv -t oracle -u developer -p PassDev -H localhost --db-sid XEPDB1 -o reporte.pdf"
-    subprocess.call(comando, shell=True)
     cursor.close()
     connection.close()
+    jrxml = './jrxml/QuerryAcuario.jrxml'
+    outpt = './output/1.pdf'
+    data = './temp.csv'
+    datareport_csv(jrxml_filename=jrxml, output_filename=outpt, data_csv=data)
+    ruta_pdf = os.path.abspath('./output/1.pdf')
+    abrir_pdf_en_navegador(ruta_pdf)
 
-    print("Reporte generado con éxito.")
+
+def reporte_peligro_extincion(estado_extincion):
+    connection = create_connection()
+    cursor = connection.cursor()
+
+    query = str(f'''SELECT NOMBRE_CIENTIFICO AS ESPECIE_AMENAZADA
+                    FROM DEVELOPER.ANIMAL
+                    JOIN DEVELOPER.PELIGRO USING(ID_PELIGRO)
+                    JOIN DEVELOPER.ESPECIE USING(ID_ESPECIE)
+                    WHERE PELIGRO LIKE \'{estado_extincion}\'
+                    GROUP BY NOMBRE_CIENTIFICO''')
+
+    cursor.execute(query)
+    resultados = cursor.fetchall()
+    archivo_csv = 'temp.csv'
+    with open(archivo_csv, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['NOMBRE_CIENTIFICO'])
+        for row in resultados:
+            writer.writerow(row)
+
+    cursor.close()
+    connection.close()
+    jrxml = './jrxml/2.jrxml'
+    outpt = './output/2.pdf'
+    data = './temp.csv'
+    datareport_csv(jrxml_filename=jrxml, output_filename=outpt, data_csv=data)
+    ruta_pdf = os.path.abspath('./output/2.pdf')
+    abrir_pdf_en_navegador(ruta_pdf)
 
 
-    # jrxml_file = open(f'./Files/QuerryAcuario.jrxml', 'w')
-    # jrxml_file.close()
-    ruta_jrxml = "Files/QuerryAcuario.jrxml"
-    generar_reporte(ruta_jrxml)
+def reporte_cantidad_animales():
+    connection = create_connection()
+    cursor = connection.cursor()
+
+    query = str(f'''SELECT NOMBRE_HABITAT, COUNT(NOMBRE_ANIMAL) AS ANIMALES
+                    FROM DEVELOPER.ANIMAL
+                    JOIN DEVELOPER.HABITAT USING(ID_HABITAT)
+                    GROUP BY NOMBRE_HABITAT
+                    ORDER BY ANIMALES''')
+
+    cursor.execute(query)
+    resultados = cursor.fetchall()
+    archivo_csv = 'temp.csv'
+    with open(archivo_csv, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['NOMBRE_HABITAD', 'ANIMALES'])
+        for row in resultados:
+            writer.writerow(row)
+
+    cursor.close()
+    connection.close()
+    jrxml = './jrxml/4.jrxml'
+    outpt = './output/4.pdf'
+    data = './temp.csv'
+    datareport_csv(jrxml_filename=jrxml, output_filename=outpt, data_csv=data)
+    ruta_pdf = os.path.abspath('./output/4.pdf')
+    abrir_pdf_en_navegador(ruta_pdf)
+
+
+def datareport_csv(jrxml_filename, output_filename, data_csv):
+
+    logger.info('running datasource csv sample')
+
+    # CSV datasource configuration
+    data_config = {'adapter': 'csv', 'filename': data_csv}
+
+    pdf_page = BasicReport(jrxml_filename=jrxml_filename, output_filename=output_filename, data_config=data_config)
+    pdf_page.generate_report()
+
+
+def abrir_pdf_en_navegador(ruta_pdf):
+    url = ruta_pdf
+    webbrowser.open_new_tab(url)
+
+
+# reporte_habitad("Reptiles")
+# reporte_peligro_extincion("Amenzada")
+# reporte_cantidad_animales()
